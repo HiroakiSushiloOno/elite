40352ea7d0481e6e3c9a6a64a755668a8230c766
HiroakiOno <sushilo0613@gmail.com>
2019-03-21 11:29:34 -0700

    Fixed an animation movements

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@
     }
   });
   $('.back-to-top').click(function(){
-    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    $('html, body').animate({scrollTop : 1350},1500, 'easeInOutExpo');
     return false;
   });
 
@@ -80,11 +80,11 @@
 
   $(window).on('scroll', function () {
     var cur_pos = $(this).scrollTop();
-  
+
     nav_sections.each(function() {
       var top = $(this).offset().top - main_nav_height,
           bottom = top + $(this).outerHeight();
-  
+
       if (cur_pos >= top && cur_pos <= bottom) {
         main_nav.find('li').removeClass('active');
         main_nav.find('a[href="#'+$(this).attr('id')+'"]').parent('li').addClass('active');
@@ -106,7 +106,7 @@
     $('#portfolio-flters li').on( 'click', function() {
       $("#portfolio-flters li").removeClass('filter-active');
       $(this).addClass('filter-active');
-  
+
       portfolioIsotope.isotope({ filter: $(this).data('filter') });
     });
   });
@@ -120,4 +120,3 @@
   });
 
 })(jQuery);
-
